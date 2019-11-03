9bbdad3ddaef0ef8033cb7b16638b914a54c3ff4
Iqra Naz <iqranaz240gmail.com>
2019-11-03 22:46:14 +0500

    My Assignment# 1

---
+++ Assignment#1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a="Twinkle, twinkle, little star,\n \t How I wonder what you are!"
+b="\t \t Up above the world so high, \n \t \t Like a diamond in the sky."
+print(a)
+print(b)
+print(a)
+
+
+# In[3]:
+
+
+import sys
+print("Python Version")
+print(sys.version)
+
+
+# In[5]:
+
+
+import datetime
+print("Date and Time")
+now=datetime.datetime.now()
+print(now)
+
+
+# In[6]:
+
+
+rad=int(input("Enter the radius of Circle:"))
+area=(22/7)*float(rad**2)
+print("Area of Circle is: ",area)
+
+
+# In[8]:
+
+
+fname=input("Enter your First Name: ")
+lname=input("Enter your Last Name: ")
+print(lname + " " + fname)
+
+
+# In[10]:
+
+
+a=int(input("Enter first no.: "))
+b=int(input("Enter second no.: "))
+c=a+b
+print("Sum of two no. is:" ,c)
+
+
+# In[ ]:
+
+
+
+
